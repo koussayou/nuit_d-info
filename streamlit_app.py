c044c8d1712e1c14ff0d291d66bec3adb4efaa81
koussayou <43079763+koussayou@users.noreply.github.com>
2023-12-08 04:13:56 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import ee
-ee.Authenticate()
+service_account = 'test1-783@openmap-407503.iam.gserviceaccount.com'
+credentials = ee.ServiceAccountCredentials(service_account, './openmap-407503')
+ee.Initialize(credentials)
 ee.Initialize()
 print(ee.Image("NASA/NASADEM_HGT/001").get("title").getInfo())
 
